1457c21f331da99cb2cdeabdba22bf979b438bdf
Виталий <vitalijshonov@gmail.com>
2019-10-14 19:35:11 +0300

    add dependencies vuex, router, axios, pug, scss

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  include ../tools/mixins.pug
+  .home
+    img(alt="Vue logo" src="@/assets/logo.png")
+
+</template>
+
+<script>
+  export default {
+    name: "Home"
+  }
+</script>
+
+<style lang="scss"></style>
